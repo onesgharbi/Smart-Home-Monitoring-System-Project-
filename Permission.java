6362b4947ebcda0b0c40117db29168f5aa52a1d3
onesgharbi <223A9680@live.uem.es>
2024-11-09 18:19:17 +0100

    Update Permission.java

--- Permission.java
+++ Permission.java
@@ -1,1 +1,14 @@
+// File: Permission.java
+// Package: com.smarthome.security
+
+package com.smarthome.security;
+
+/**
+ * Enum representing various permissions in the system.
+ */
+public enum Permission {
+    ACCESS_HOMEOWNER_AREA,
+    ACCESS_SECURITY_AREA,
+    ACCESS_TECHNICIAN_AREA
+}
 
