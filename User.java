579d43bc34e891b4953e5affdd5db8c5c2e17ba5
onesgharbi <223A9680@live.uem.es>
2024-11-09 18:10:51 +0100

    User.java

--- User.java
+++ User.java
@@ -1,1 +1,29 @@
+// File: User.java
+// Package: com.smarthome.users
+
+package com.smarthome.users;
+
+public class User {
+    private final String username;
+    private final String password;
+    private final UserRole role;
+
+    public User(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+}
 
